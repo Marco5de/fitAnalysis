3ffa330f19854385d8d4d9343a158c36f1878530
marco5de <marco.deuscher@t-online.de>
2019-12-16 22:47:48 +0100

    added smoothing and general stats

--- fitparse/fitAnalyzer.py
+++ fitparse/fitAnalyzer.py
@@ -1,16 +1,19 @@
 from fitparse import FitFile, FitParseError
 import numpy as np
 import matplotlib.pylab as plt
+from scipy.ndimage.filters import gaussian_filter1d
+import numbers
 
-#todo refactoring, dass nicht alles 10x berechnet werden muss!
-#todo speichern in Datei mit tagen damit ATL,CTL,TSB berechnet werden können!
+
+# todo refactoring, dass nicht alles 10x berechnet werden muss!
+# todo speichern in Datei mit tagen damit ATL,CTL,TSB berechnet werden können!
 class fitAnalyzer:
     fitfile = 0
     numberRecords = 0
     ftp = 0
     powerVec = 0
 
-    def __init__(self, fitFilePath,ftp):
+    def __init__(self, fitFilePath, ftp):
         try:
             self.fitfile = FitFile(fitFilePath)
             self.fitfile.parse()
@@ -39,34 +42,61 @@ class fitAnalyzer:
     def __getAverage(self, vec):
         return np.average(vec)
 
+    # todo better way to do this?
+    def __getGeneralStats(self):
+        print()
+        lock = False
+        for idx, record in enumerate(self.fitfile.get_messages()):
+            if lock:
+                print("Total ascent: " + str(record.get_value("total_ascent")))
+                print("Total calories: " + str(record.get_value("total_calories")))
+                break;
+            if (isinstance(record.get_value("avg_power"), numbers.Number)):
+                print("Avg. Power: " + str(record.get_value("avg_power")))
+                print("Avg. Cadence: " + str(record.get_value("avg_cadence")))
+                print("Start time: " + str(record.get_value("start_time")))
+                print("Sport: " + str(record.get_value("sport")))
+                print("Max. Cadence: " + str(record.get_value("max_cadence")))
+                print("Max. Power: " + str(record.get_value("max_power")))
+                print("Max. Speed: " + str(record.get_value("max_speed")))
+                print("Total distance: " + str(record.get_value("total_distance")))
+                lock = True
+
+
     def __getNormalized(self):
-        #assuming there is a timestep every second --> 30 sec moving avg
+        # assuming there is a timestep every second --> 30 sec moving avg
         mvAvgLen = 30
         mvAvg = np.cumsum(self.powerVec, dtype=float)
         mvAvg[mvAvgLen:] = mvAvg[mvAvgLen:] - mvAvg[:-mvAvgLen]
         mvAvg = mvAvg[mvAvgLen - 1:] / mvAvgLen
-        return np.power(np.average(np.power(mvAvg,4)),.25)
+        return np.power(np.average(np.power(mvAvg, 4)), .25)
 
     def getIntensityFactor(self):
         return self.__getNormalized() / self.ftp
 
     def getTrainingStressScore(self):
-        #assuming one record per second!
+        # assuming one record per second!
         return (self.numberRecords * self.__getNormalized() * self.getIntensityFactor()) / (36 * self.ftp)
 
     def plotPower(self):
+        self.__getGeneralStats()
         avg = self.__getAverage(self.powerVec)
         normPower = self.__getNormalized()
 
-        plt.plot(np.arange(0, self.numberRecords), self.powerVec)
+        t = np.arange(0, self.numberRecords/60,1/60)
+        #sigma kann als parameter noch eingestellt werden, wie sehr gesmoothed werden soll (sieht mit 3 aber nicht schlecht aus)
+        smoothedPower = gaussian_filter1d(self.powerVec, sigma=3)
+
+        plt.plot(t, smoothedPower,linewidth=1)
         plt.title("Power")
         plt.text(0, 640, str("Avg. Power: " + str(int(avg)) + "Watts"), fontsize=12, color="red")
         plt.text(0, 600, str("Norm. Power: " + str(int(normPower)) + "Watts"), fontsize=12, color="red")
+        plt.xlabel("Time in min"); plt.ylabel("Power in W")
         plt.show()
 
 
-ana = fitAnalyzer("file.fit",275)
+ana = fitAnalyzer("file.fit", 275)
 print("Anzahl an Records: " + str(ana.numberRecords))
-print("Intensity Factor: "+ str(ana.getIntensityFactor()))
+print("Intensity Factor: " + str(ana.getIntensityFactor()))
 print("TSS: " + str(ana.getTrainingStressScore()))
 ana.plotPower()
