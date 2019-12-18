e6047f6ce8ff3e6d87c5829d87c1c124ce56ec8b
marco5de <marco.deuscher@t-online.de>
2019-12-18 20:45:02 +0100

    restructured fitanalyzer and created fitobject

---
+++ fitparse/FitObject.py
@@ -0,0 +1,121 @@
+import codecs
+import json
+import numbers
+
+import numpy as np
+from fitparse import FitFile, FitParseError
+import importlib
+
+importlib.import_module("FitObject")
+
+#todo speed vec missing
+
+class FitObject:
+    total_calories = total_ascent = total_distance = max_cadence = 0
+    avg_power = avg_cadence = start_time = sport = max_power = max_speed = 0
+    fitfile = numerRecords = powerVec = speedVec = cadenceVec = 0
+
+    def __init__(self, path):
+        self.__path = path
+        try:
+            self.fitfile = FitFile(self.__path)
+            self.fitfile.parse()
+        except FitParseError as e:
+            raise NameError("Error parsing/loading .FIT file")
+
+        self.numberRecords = self.__getRecordNumber()
+        self.powerVec = self.__getFieldVector("power")
+        self.speedVec = self.__getFieldVector("enhanced_speed") * 3.6
+        self.cadenceVec = self.__getFieldVector("cadence")
+        self.__getGeneralStats()
+
+    def __str__(self):
+        outstr = ""
+        outstr += ("Fitobject: " + str(self.start_time) + "\n")
+        outstr += ("Avg. Power: " + str(self.avg_power) +"Watts"+ "\n")
+        outstr += ("Distance: " + str(self.total_distance/1000) + "km" + "\n")
+        outstr += ("Ascent: " + str(self.total_ascent) +"m"+ "\n")
+        outstr += ("Calories burned: " + str(self.total_calories) +"kcal"+ "\n")
+        outstr += ("Avg. Cadence: " + str(self.avg_cadence) +"1/min"+ "\n")
+        outstr += ("Max. speed: " + str(self.max_speed*3.6) +"km/h"+ "\n")
+        outstr += ("Max. Power: " + str(self.max_power) +"Watts"+ "\n")
+        outstr += ("Max. Cadence: " + str(self.max_cadence) +"1/min"+ "\n")
+        return outstr
+
+    def __getRecordNumber(self):
+        count = 0
+        for record in self.fitfile.get_messages('record'):
+            count += 1
+        return count
+
+    def __getFieldVector(self, fieldString):
+        vec = np.zeros(self.numberRecords)
+        i = 0
+        for record in self.fitfile.get_messages('record'):
+            vec[i] = record.get_value(fieldString)
+            i += 1
+        return vec
+
+    def __getNormalized(self):
+        # assuming there is a timestep every second --> 30 sec moving avg
+        mvAvgLen = 30
+        mvAvg = np.cumsum(self.powerVec, dtype=float)
+        mvAvg[mvAvgLen:] = mvAvg[mvAvgLen:] - mvAvg[:-mvAvgLen]
+        mvAvg = mvAvg[mvAvgLen - 1:] / mvAvgLen
+        return np.power(np.average(np.power(mvAvg, 4)), .25)
+
+
+    def __getGeneralStats(self):
+        lock = False
+        for idx, record in enumerate(self.fitfile.get_messages()):
+            if lock:
+                self.total_ascent = record.get_value("total_ascent")
+                self.total_calories = record.get_value("total_calories")
+                break;
+            if (isinstance(record.get_value("avg_power"), numbers.Number)):
+                self.avg_power = record.get_value("avg_power")
+                self.avg_cadence = record.get_value("avg_cadence")
+                self.start_time = record.get_value("start_time")
+                self.sport = record.get_value("sport")
+                self.max_cadence = record.get_value("max_cadence")
+                self.max_power = record.get_value("max_power")
+                self.max_speed = record.get_value("max_speed")
+                self.total_distance = record.get_value("total_distance")
+                lock = True
+
+    def serialize(self,outPath):
+        data = {}
+        data["ascent"] = self.total_ascent
+        data["calories"] = self.total_calories
+        data["totalDistance"] = self.total_distance
+        data["max_speed"] = self.max_speed
+        data["max_power"] = self.max_power
+        data["max_candence"] = self.max_cadence
+        data["sport"] = self.sport
+        data["avg_cadence"] = self.avg_cadence
+        data["avg_power"] = self.avg_power
+        data["speedVec"] = self.speedVec.tolist()
+        data["powerVec"] = self.powerVec.tolist()
+        data["cadenceVec"] = self.cadenceVec.tolist()
+        data["start_time"] = str(self.start_time)
+
+        json.dump(data, codecs.open(outPath, 'w', encoding='utf-8'), separators=(',', ':'),
+                  sort_keys=True, indent=4)
+
+    def deserialize(self, path):
+        data = json.loads(codecs.open(path, 'r', encoding='utf-8').read())
+        self.total_ascent = data["ascent"]
+        self.total_calories = data["calories"]
+        self.total_distance = data["totalDistance"]
+        self.max_speed = data["max_speed"]
+        self.max_power = data["max_power"]
+        self.max_cadence = data["max_candence"]
+        self.sport = data["sport"]
+        self.avg_cadence = data["avg_cadence"]
+        self.avg_power = data["avg_power"]
+        self.speedVec = np.array(data["speedVec"])
+        self.powerVec = np.array(data["powerVec"])
+        self.cadenceVec = np.array(data["cadenceVec"])
+        self.start_time = data["start_time"]
+
+
--- fitparse/fitAnalyzer.py
+++ fitparse/fitAnalyzer.py
@@ -2,76 +2,22 @@ from fitparse import FitFile, FitParseError
 import numpy as np
 import matplotlib.pylab as plt
 from scipy.ndimage.filters import gaussian_filter1d
-import numbers
 import cv2
-import codecs, json 
+import FitObject
 
 
 # todo speichern in Datei mit tagen damit ATL,CTL,TSB berechnet werden können!
 # todo speichern des datums etc--> serialisierung des objekts und hinzufügen einer liste! --> möglichst so, dass ein fehler nicht alles kaputt macht
 class fitAnalyzer:
-    total_calories=total_ascent=total_distance=max_cadence = 0
-    avg_power=avg_cadence=start_time=sport=max_power=max_speed = 0
-    
-    def __init__(self, fitFilePath, ftp):
-        try:
-            self.fitfile = FitFile(fitFilePath)
-            self.fitfile.parse()
-        except FitParseError as e:
-            raise NameError("Error parsing/loading .FIT file")
 
-        self.numberRecords = self.__getRecordNumber()
+    def __init__(self, ftp, fitobj):
         self.ftp = ftp
-        self.powerVec = self.__getFieldVector("power")
-        self.speedVec = self.__getFieldVector("enhanced_speed") * 3.6
-        self.cadenceVec = self.__getFieldVector("cadence")
-        self.__getGeneralStats()
-
-    def __str__(self):
-        return "Hier sollte ein sinnvoller string ausgegebn werden!"
-
-    # todo is there an efficient way to do this?
-    def __getRecordNumber(self):
-        count = 0
-        for record in self.fitfile.get_messages('record'):
-            count += 1
-        return count
-
-    def __getFieldVector(self, fieldString):
-        vec = np.zeros(self.numberRecords)
-        i = 0
-        for record in self.fitfile.get_messages('record'):
-            vec[i] = record.get_value(fieldString)
-            i += 1
-        return vec
-
-    def __getAverage(self, vec):
-        return np.average(vec)
-
-    # todo better way to do this?
-    def __getGeneralStats(self):
-        lock = False
-        for idx, record in enumerate(self.fitfile.get_messages()):
-            if lock:
-                self.total_ascent = record.get_value("total_ascent")
-                self.total_calories = record.get_value("total_calories")
-                break;
-            if (isinstance(record.get_value("avg_power"), numbers.Number)):
-                self.avg_power = record.get_value("avg_power")
-                self.avg_cadence = record.get_value("avg_cadence")
-                self.start_time = record.get_value("start_time")
-                self.sport = record.get_value("sport")
-                self.max_cadence = record.get_value("max_cadence")
-                self.max_power = record.get_value("max_power")
-                self.max_speed = record.get_value("max_speed")
-                self.total_distance = record.get_value("total_distance")
-                lock = True
-
+        self.fitobj = fitobj;
 
     def __getNormalized(self):
         # assuming there is a timestep every second --> 30 sec moving avg
         mvAvgLen = 30
-        mvAvg = np.cumsum(self.powerVec, dtype=float)
+        mvAvg = np.cumsum(self.fitobj.powerVec, dtype=float)
         mvAvg[mvAvgLen:] = mvAvg[mvAvgLen:] - mvAvg[:-mvAvgLen]
         mvAvg = mvAvg[mvAvgLen - 1:] / mvAvgLen
         return np.power(np.average(np.power(mvAvg, 4)), .25)
@@ -81,35 +27,35 @@ class fitAnalyzer:
 
     def getTrainingStressScore(self):
         # assuming one record per second!
-        return (self.numberRecords * self.__getNormalized() * self.getIntensityFactor()) / (36 * self.ftp)
+        return (self.fitobj.numberRecords * self.__getNormalized() * self.getIntensityFactor()) / (36 * self.ftp)
 
     def plotPower(self):
-        avg = self.__getAverage(self.powerVec)
+        avg = np.average(self.fitobj.powerVec)
         normPower = self.__getNormalized()
         print("Avg. Power: " + str(avg) + " Norm. Power: " + str(normPower))
         #sigma kann als parameter noch eingestellt werden, wie sehr gesmoothed werden soll (sieht mit 3 aber nicht schlecht aus)
-        smoothedPower = gaussian_filter1d(self.powerVec, sigma=3)
+        smoothedPower = gaussian_filter1d(self.fitobj.powerVec, sigma=3)
         self.__plot(smoothedPower,"Power","Time in min","Power in Watts")
 
 
     def plotSpeed(self):
-        smoothedSpeed = gaussian_filter1d(self.speedVec,sigma=3)
+        smoothedSpeed = gaussian_filter1d(self.fitobj.speedVec,sigma=3)
         self.__plot(smoothedSpeed,"Speed","Time in min","Speed in km/h")
 
     def plotCadence(self):
-        smoothedCadence = gaussian_filter1d(self.cadenceVec,sigma=3)
+        smoothedCadence = gaussian_filter1d(self.fitobj.cadenceVec,sigma=3)
         self.__plot(smoothedCadence,"Cadence","Time in min","Cadence in 1/min")
 
-    def __plot(self,plotVec,title,xlabel,ylabel):
-        t = np.arange(0, self.numberRecords / 60, 1 / 60)
-        plt.plot(t, plotVec, linewidth=1)
+    def __plot(self,plotVec,title,xlabel,ylabel,axis=0):
+        axis = np.arange(0, self.fitobj.numberRecords / 60, 1 / 60)
+        plt.plot(axis, plotVec, linewidth=1)
         plt.title(title)
         plt.xlabel(xlabel);
         plt.ylabel(ylabel)
         plt.show()
     
     def calcKmeans(self):
-        data = np.transpose(np.vstack((self.cadenceVec,self.powerVec)))
+        data = np.transpose(np.vstack((self.fitobj.cadenceVec,self.fitobj.powerVec)))
         data = np.float32(data)
         criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 100, 0.1)
         compactness,labels,centers = cv2.kmeans(data,5,None,criteria,10,flags=cv2.KMEANS_RANDOM_CENTERS)
@@ -118,34 +64,25 @@ class fitAnalyzer:
         print("Centers: " + str(centers))
 
     def plotPowerCadence(self):
-        colors = np.random.rand(self.numberRecords)
-        smoothedCadence = gaussian_filter1d(self.cadenceVec,sigma=3)
-        smoothedPower = gaussian_filter1d(self.powerVec,sigma=3)
+        colors = np.random.rand(self.fitobj.numberRecords)
+        smoothedCadence = gaussian_filter1d(self.fitobj.cadenceVec,sigma=3)
+        smoothedPower = gaussian_filter1d(self.fitobj.powerVec,sigma=3)
         plt.scatter(smoothedCadence,smoothedPower,c=colors,alpha=.5,s=2)
         plt.title("Power-Cadence")
         plt.show()
     
-    def serialize(self):
-        powerList = self.powerVec.tolist() # nested lists with same data, indices
-        file_path = "ser.json" ## your path variable
-        json.dump({"powerVec" : powerList}, codecs.open(file_path, 'w', encoding='utf-8'), separators=(',', ':'), sort_keys=True, indent=4)
-    
-    def deserialize(self,path):
-        obj_text = codecs.open(path, 'r', encoding='utf-8').read()
-        b_new = json.loads(obj_text)
-        a_new = np.array(b_new)
-        #print(a_new)
+
 
 print("Opencv successfully installed: Version: " + str(cv2.__version__))
-ana = fitAnalyzer("file.fit", 275)
-print("Anzahl an Records: " + str(ana.numberRecords))
+obj = FitObject.FitObject("file.fit")
+ana = fitAnalyzer( 275,obj)
+
 print("Intensity Factor: " + str(ana.getIntensityFactor()))
 print("TSS: " + str(ana.getTrainingStressScore()))
-#ana.plotPowerCadence()
-#ana.plotPower()
-#ana.plotSpeed()
-#ana.plotCadence()
+ana.plotPowerCadence()
+ana.plotPower()
+ana.plotSpeed()
+ana.plotCadence()
 ana.calcKmeans()
-print("ToString: " + str(ana))
-ana.serialize()
-ana.deserialize("ser.json")
+
+
